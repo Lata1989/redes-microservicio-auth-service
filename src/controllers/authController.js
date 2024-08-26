9a100ebde8016109fbddbaa5861c7b00386cd464
Lata1989 <pabloalejandrodelaiglesia@gmail.com>
2024-08-25 22:08:05 -0300

    registro corregido

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,30 +2,28 @@ import jwt from 'jsonwebtoken';
 import { getDB } from '../config/database.js';
 import { userCollection, jwtSecret, jwtExpiresIn } from '../config/envConfig.js';
 
-// Registrar un nuevo usuario y generar un token JWT
+// Registrar un nuevo usuario
 export async function registerUser(req, res) {
-  const { username, password } = req.body;
-
-  try {
-    const db = getDB();
-    const usersCollection = db.collection(userCollection);
-
-    const existingUser = await usersCollection.findOne({ username });
-    if (existingUser) {
-      return res.status(400).json({ error: 'El nombre de usuario ya existe' });
+    const { username, password } = req.body;
+  
+    try {
+      const db = getDB();
+      const usersCollection = db.collection(userCollection);
+  
+      const existingUser = await usersCollection.findOne({ username });
+      if (existingUser) {
+        return res.status(400).json({ error: 'El nombre de usuario ya existe' });
+      }
+  
+      const newUser = { username, password, createdAt: new Date(), updatedAt: new Date() };
+      await usersCollection.insertOne(newUser);
+  
+      // No se está generando ni devolviendo el token en esta versión del código
+      res.status(201).json({ message: 'Usuario registrado exitosamente' });
+    } catch (error) {
+      res.status(500).json({ error: 'No se pudo registrar el usuario' });
     }
-
-    const newUser = { username, password, createdAt: new Date(), updatedAt: new Date() };
-    await usersCollection.insertOne(newUser);
-
-    // Se estaba generando el token pero no se devolvía en la respuesta
-    const token = jwt.sign({ username }, jwtSecret, { expiresIn: jwtExpiresIn });
-
-    res.status(201).json({ message: 'Usuario registrado exitosamente', token }); // Se devolvía el token aquí
-  } catch (error) {
-    res.status(500).json({ error: 'No se pudo registrar el usuario' });
   }
-}
 
 // Autenticar un usuario y generar un token JWT
 export async function authenticateUser(req, res) {
